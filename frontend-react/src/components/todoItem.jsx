008452313145140693f602f9468b7330990a0d7a
Harrison Avella <harrisonavella@gmail.com>
2021-07-22 23:00:49 -0500

    fixed

--- frontend-react/src/components/todoItem.jsx
+++ frontend-react/src/components/todoItem.jsx
@@ -4,20 +4,21 @@ const TodoItem = (props) => {
   const [todoItem, setTodoItem] = useState(props.data);
   const [isDirty, setDirty] = useState(false);
   useEffect(() => {
-    if(isDirty) {
-    fetch(`http://localhost:8080/api/todoitems/${todoItem.id}`, {
-      mode: "no-cors",
-      method: "PUT",
-      headers: {
-        "content-type": "application/json",
-      },
-      body: JSON.stringify(todoItem),
-    })
-      .then((response) => response.json())
-      .then((json) => {
-        setTodoItem(json);
-      });
-    //console.log("Cambio el estado de :", todoItem);
+    if (isDirty) {
+      fetch(`http://localhost:8080/api/todoItems/${todoItem.id}`, {
+        //mode: "no-cors",
+        method: 'PUT',
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify(todoItem),
+      })
+        .then((response) => response.json())
+        .then((json) => {
+          setDirty(false);
+          setTodoItem(json);
+        });
+      console.log("Cambio el estado de :", todoItem);
     }
   }, [todoItem, isDirty]);
 
@@ -28,7 +29,7 @@ const TodoItem = (props) => {
         checked={todoItem.isDone}
         onChange={() => {
           setDirty(true);
-          setTodoItem({ ...todoItem, idDone: !todoItem.idDone })
+          setTodoItem({ ...todoItem, isDone: !todoItem.isDone });
         }}
       />
       <span>{todoItem.task}</span>
